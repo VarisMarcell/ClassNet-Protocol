833fbdcafae289f8d2fc02883ba7698de73362a4
ROBLOXLuaScripter <sjerufous@gmail.com>
2022-12-06 22:40:46 -0500

    idk but I made the server work

--- server.py
+++ server.py
@@ -12,15 +12,22 @@ new_session = False
 
 os.chdir(os.path.join(".", "Sessions"))
 
-random_number = random.randint(0, 1000)
+random_number = random.randint(0, 10)
 
 session_key = str(random_number)
 
 def newSession(name):
-    
-    newFile = open(f"{name}.txt", "w")
+    x = False
+    for i in os.listdir():
+        print(i)
+        if i.endswith('.txt'):
+            if i != (f"{name}.txt"):
+                x = True
+    if x:
+        newFile = open(f"{name}.txt", "w")
+    return newFile if x else 3
         
 if __name__ == "__main__":
     new_session = True
-#     newSession(session_key)
-
+    newSession(session_key)
+    
