c13d7c35e9ff75c6c449b1be596c950c8e71c8d0
ROBLOXLuaScripter <sjerufous@gmail.com>
2022-12-06 20:48:54 -0500

    a thing that does something

--- server.py
+++ server.py
@@ -4,3 +4,23 @@
 # CPTR-215
 # 12/6/2022 - first draft
 
+import random
+import os
+
+
+new_session = False
+
+os.chdir(os.path.join(".", "Sessions"))
+
+random_number = random.randint(0, 1000)
+
+session_key = str(random_number)
+
+def newSession(name):
+    
+    newFile = open(f"{name}.txt", "w")
+        
+if __name__ == "__main__":
+    new_session = True
+#     newSession(session_key)
+
