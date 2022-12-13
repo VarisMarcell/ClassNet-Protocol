bea682b92acc905c0ee972e2d2176aa06b125c47
MSI\AV <varismarcell@gmail.com>
2022-12-13 13:51:47 -0500

    i added some stuff to backend ;-; lmk what i can do later...

--- server.py
+++ server.py
@@ -3,31 +3,52 @@
 # Caeden Scott
 # CPTR-215
 # 12/6/2022 - first draft
+# 12/9/2022 - another version
+# 12/13/2022 - rreeeeee
 
 import random
 import os
+import uuid
 
+
+# random information that needs to be auto-generated yet unique
+session_key = str(uuid.uuid4())[:6]
+admin_key = str(uuid.uuid4())
 
 new_session = False
 
 os.chdir(os.path.join(".", "Sessions"))
 
-random_number = random.randint(0, 10)
+def newSession(sessionName, adminKey):
+    x = True
+    y = False
+    num = 0
+    tempFile = 'z'
+    while x:
+        num += 1
+        for i in os.listdir():
+            if i == (f"{sessionName}.txt"):
+                return
+            else:
+                y = True
+        if os.listdir() == [] or y:
+            x = False
 
-session_key = str(random_number)
+        if x == False:
+            with open(f"{sessionName}.txt", "w") as newFile:
+                newFile.write(adminKey)
+                newFile.write('\n')
+                tempFile = newFile
+        print(num)
 
-def newSession(name):
-    x = False
-    for i in os.listdir():
-        print(i)
-        if i.endswith('.txt'):
-            if i != (f"{name}.txt"):
-                x = True
-    if x:
-        newFile = open(f"{name}.txt", "w")
-    return newFile if x else 3
-        
+    return tempFile
+
+
+
 if __name__ == "__main__":
-    new_session = True
-    newSession(session_key)
+    import doctest
+    doctest.testmod()
+
+    newSession(session_key, admin_key)
+
     
