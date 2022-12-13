13de11bbdb59edf869f293c16cdccd615f95e0c1
CaedenScott <114004469+CaedenScott@users.noreply.github.com>
2022-12-12 19:28:07 -0500

    Front Page

--- ClassNetBackend/backendcode.py
+++ ClassNetBackend/backendcode.py
@@ -18,3 +18,6 @@ def home():
         "name": "Ryan"
     }
     return response_body
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- ClassNetReact/src/App.jsx
+++ ClassNetReact/src/App.jsx
@@ -3,30 +3,30 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState({name:""})
+  const [data, setData] = useState({ name: "" })
 
   function getData() {
     fetch('http://127.0.0.1:5000', {
-      'methods':"GET",
-      headers : {
-        'Content-Type':"application/json",
+      'methods': "GET",
+      headers: {
+        'Content-Type': "application/json",
         'Accept': "application/json"
       }
     })
-    .then(response => response.json())
-    .then(response => setData(response))
-    .catch(error => console.log(error))
+      .then(response => response.json())
+      .then(response => setData(response))
+      .catch(error => console.log(error))
 
     fetch('http://127.0.0.1:5000/Caeden', {
-      'methods':"GET",
-      headers : {
-        'Content-Type':"application/json",
+      'methods': "GET",
+      headers: {
+        'Content-Type': "application/json",
         'Accept': "application/json"
       }
     })
-    .then(response => response.json())
-    .then(response => setData(response))
-    .catch(error => console.log(error))
+      .then(response => response.json())
+      .then(response => setData(response))
+      .catch(error => console.log(error))
   }
 
   useEffect(() => {
@@ -34,7 +34,11 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <h1>{data.name}</h1>
+      <h1 className='mainTitle'>ClassNet Protocols</h1>
+      <section className='mainButtons'>
+        <button className='createSession'>Create Session</button>
+        <button className='joinSession'>Join Session</button>
+      </section>
     </div>
   )
 }
